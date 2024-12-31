92569a7c5a3da1cf58ae5468acc306ee8938fefa
Your Name <your-email@example.com>
2024-12-31 21:44:32 +0100

    Premier commit

---
+++ src/main/java/com/focus/controllers/SignupController.java
@@ -0,0 +1,25 @@
+package com.focus.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignupController {
+
+    @FXML
+    private TextField emailField;
+    
+    @FXML
+    private PasswordField passwordField;
+    
+    @FXML
+    private Button signupButton;
+
+   
+    @FXML
+    public void handleSignInClick() {
+        // Your sign-in logic here
+    }
+}
+
